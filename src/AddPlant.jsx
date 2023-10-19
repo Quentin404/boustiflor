38f6c7b62d0025cfc8fc7fbe4aab4898991e6383
AxelDona <axel.dona@outlook.com>
2023-10-19 13:08:21 +0200

    add plant

---
+++ src/AddPlant.jsx
@@ -0,0 +1,95 @@
+import {db} from "./firebase/firebase";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {addDoc, collection} from "firebase/firestore";
+
+export function AddPlant() {
+
+    const [sName, setSName] = useState('');
+    const [vName, setVName] = useState('');
+    const [family, setFamily] = useState('');
+    const [toxicSubstances, setToxicSubstances] = useState('');
+    const [toxicOrgans, setToxicOrgans] = useState('');
+    const [symptoms, setSymptoms] = useState('');
+    const [proneSpecies, setProneSpecies] = useState('');
+    const [isPending, setIsPending] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setIsPending(true);
+
+        // Add a new document in collection "cities"
+        addDoc(collection(db, "plants"), {
+            vernacularName: vName,
+            scientificName: sName,
+            family: family,
+            toxicSubstances: toxicSubstances,
+            toxicOrgans: toxicOrgans,
+            symptoms: symptoms,
+            proneSpecies: proneSpecies
+        });
+
+        setIsPending(false);
+
+        navigate('/');
+    }
+
+    return (
+
+        <form onSubmit={handleSubmit}>
+            <label>Nom vernaculaire :</label>
+            <input
+                type="text"
+                required
+                value={vName}
+                onChange={(e) => setVName(e.target.value)}
+            />
+            <label>Nom scientifique :</label>
+            <input
+                type="text"
+                required
+                value={sName}
+                onChange={(e) => setSName(e.target.value)}
+            />
+            <label>Famille :</label>
+            <input
+                type="text"
+                required
+                value={family}
+                onChange={(e) => setFamily(e.target.value)}
+            />
+            <label>Substances toxiques :</label>
+            <input
+                type="text"
+                required
+                value={toxicSubstances}
+                onChange={(e) => setToxicSubstances(e.target.value)}
+            />
+            <label>Organes toxiques :</label>
+            <input
+                type="text"
+                required
+                value={toxicOrgans}
+                onChange={(e) => setToxicOrgans(e.target.value)}
+            />
+            <label>Symptômes :</label>
+            <input
+                type="text"
+                required
+                value={symptoms}
+                onChange={(e) => setSymptoms(e.target.value)}
+            />
+            <label>Espèces vulnérables :</label>
+            <input
+                type="text"
+                required
+                value={proneSpecies}
+                onChange={(e) => setProneSpecies(e.target.value)}
+            />
+            { !isPending && <button>Add Plant</button>}
+            { isPending && <button disabled>Adding Plant...</button>}
+        </form>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from "react";
 import Home from './Home';
+import {BrowserRouter as Router, Link} from "react-router-dom";
+import {Route} from "react-router-dom";
+import {Routes} from "react-router-dom";
+import {AddPlant} from "./AddPlant";
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Boustiflor</h1>
-        <span>Une filiale Toxiplants</span>
-      </header>
-      <Home />
-    </div>
+      <Router>
+          <div className="App">
+              <header className="App-header">
+                  <h1>Boustiflor</h1>
+                  <span>Une filiale Toxiplants</span>
+              </header>
+              <nav>
+                  <Link to="/">Accueil</Link>
+                  <Link to="/add">Ajouter une plante</Link>
+              </nav>
+              <div className="content">
+                  <Routes>
+                      <Route exact path="/" element={<Home/>}/>
+                      <Route exact path="/add" element={<AddPlant/>}/>
+                  </Routes>
+              </div>
+          </div>
+      </Router>
   );
 }
 
