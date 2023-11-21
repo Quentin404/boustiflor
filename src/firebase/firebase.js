9efcdb0fac5267b800d9eaf8647a82d579e5260a
sukai <quentinhuet4500@gmail.com>
2023-11-21 15:38:34 +0100

    working sign in and sign out :)

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
 
@@ -14,4 +15,10 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export { auth };
+
 export const db = getFirestore(app);
